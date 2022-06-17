2fc718aa502a9a6ee469b314c8143420e20081f6
Mukul4git <mukulhooda2002@gmail.com>
2022-06-16 20:46:23 -0400

    Add project files.

---
+++ Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,102 @@
+﻿using Lab2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        //Creating a static list for Categories
+        static List<CategoryModel> CatList = new List<CategoryModel> 
+        {
+
+            new CategoryModel { CategoryId = 1, CategoryName = "Food"},
+            new CategoryModel { CategoryId = 2, CategoryName = "Tech"},
+            new CategoryModel { CategoryId = 3, CategoryName = "Sports"}
+
+        };
+
+        // GET: CategoriesController
+        public ActionResult Index()
+        {
+            return View(CatList);
+        }
+
+        // GET: CategoriesController/Details/5
+        public ActionResult Details(int id)
+        {
+
+            var selected = CatList.First<CategoryModel>(x => x.CategoryId == id);
+            ViewBag.Message = "You selected category: " + selected.CategoryName;
+            return View(selected);
+
+        }
+
+
+
+        // GET: CategoriesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CategoriesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CategoriesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
